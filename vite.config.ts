5a917220047edd28befa62472957c5ffc311db93
MANIKUMAR <shawred.analytics@gmail.com>
2026-02-01 12:54:49 +0530

    Fix: Remove crossorigin from script tag via vite config to resolve Streamlit iframe CORS issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile(), {
+    name: 'remove-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(' type="module" crossorigin', ' type="module"');
+    }
+  }],
   build: {
     assetsInlineLimit: 100000000, // Inline all assets up to 100MB
   },
