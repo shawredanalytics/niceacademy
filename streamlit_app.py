25b3663a3db223aa0980b662d22239c4e40b6e7b
MANIKUMAR <shawred.analytics@gmail.com>
2026-02-01 22:37:42 +0530

    Add certificate info to landing page

--- streamlit_app.py
+++ streamlit_app.py
@@ -269,6 +269,8 @@ def show_landing_page():
         st.info("""
         **Welcome!** This portal allows healthcare professionals to assess their knowledge of quality systems and key patient safety protocols.
         
+        Healthcare Professionals who pass the competency assessment will be provided with free competency based self assessment certificate to help them in their career progression.
+
         **Assessment Rules:**
         - **10 Questions** per module.
         - **15 Seconds** time limit per question.
